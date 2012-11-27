99bb3fc523f6161b5bf7f755e4f54e99a2718950
Jérémie Vincke <jeremie_vincke@hotmail.com>
2012-11-27 21:19:08 +0100

    Jerem : app. Android 2.3.3 VERSION 3
    Fonctionnel :
    -> connexion sur un serveur sur base de son ip
    -> timeout
    -> affichage de l'ip quand connecté

--- Workspace/myAndroidApp/src/com/example/myandroidapp/MainActivity.java
+++ Workspace/myAndroidApp/src/com/example/myandroidapp/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.myandroidapp;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -17,44 +17,20 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		try {
-
-            //URL url = new URL("http://"+params[0]);
-			URL url = new URL("http://"+"74.125.132.94");
-            System.out.println("url created");
-
-            HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
-            urlc.setRequestProperty("User-Agent", "Android Application:"+"v1");
-            urlc.setRequestProperty("Connection", "close");
-            urlc.setConnectTimeout(1000 * 30); // mTimeout is in seconds
-            System.out.println("trying to connect...");
-            urlc.connect();
-            if (urlc.getResponseCode() == 200) {
-            	//	Main.Log("getResponseCode == 200");
-            	System.out.println("got response 200");
+		
             	
-            	View text_ip = findViewById(R.id.text_ip);
-            	while(text_ip.toString().length() == 0)
-            	if(text_ip.toString().length() != 0) {
-            		System.out.println("length diff 0");
-            		System.out.println("length diff 0");
-            		System.out.println("length diff 0");
-            		System.out.println("length diff 0");
-            	}
+            	View   text_ip        = findViewById(R.id.text_ip);
+            	View   button_connect = findViewById(R.id.button_connect);
+            	View   text_connected = findViewById(R.id.text_connected);
             	
-            	View v = findViewById(R.id.button_connect);
-				v.setVisibility(View.GONE);
-            	v = findViewById(R.id.text_connected);
-            	v.setVisibility(View.VISIBLE);
+            	View   progressCircle = findViewById(R.id.progressCircle);
             	
-            }
-		} catch (MalformedURLException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-			} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            	progressCircle.setVisibility(View.INVISIBLE);
+            	text_connected.setVisibility(View.INVISIBLE);
+            	
+            	
+            	button_connect.setOnClickListener(clickListener);
+            	           	
 	}
 
 	@Override
@@ -63,5 +39,61 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	OnClickListener clickListener = new OnClickListener() {
+	    
+	    public void onClick(View v) {
+	    	
+	        System.out.println("CLICKED");
+	        TextView text_ip = (TextView)findViewById(R.id.text_ip);
+	        View progressCircle = findViewById(R.id.progressCircle);
+	        
+	        if(connect(text_ip.getText().toString(), progressCircle)) {
+	        
+	        	View button_connect = findViewById(R.id.button_connect);
+	        	button_connect.setVisibility(View.GONE);
+	        	View text_connected = findViewById(R.id.text_connected);
+            	text_connected.setVisibility(View.VISIBLE);
+            	TextView text_ip_connected = (TextView)findViewById(R.id.text_ip_connected);
+            	text_ip_connected.setVisibility(View.VISIBLE);
+            	
+            	text_ip_connected.setText(text_ip.getText());
+	        }
+	    }
+	};
+	
+	public boolean connect(String ip, View circle) {
+		
+		try {
 
+			// Google's IP : 74.125.132.94
+            //URL url = new URL("http://"+params[0]);
+			URL url = new URL("http://"+ip);
+            System.out.println("url created");
+
+            HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+            urlc.setRequestProperty("User-Agent", "Android Application:"+"v1");
+            urlc.setRequestProperty("Connection", "close");
+            urlc.setConnectTimeout(1000 * 3); // mTimeout is in seconds
+            System.out.println("trying to connect...");
+            circle.setVisibility(View.VISIBLE);
+            urlc.connect();
+            if (urlc.getResponseCode() == 200) {
+            	//	Main.Log("getResponseCode == 200");
+            	System.out.println("got response 200");
+            	circle.setVisibility(View.INVISIBLE);
+            	urlc.disconnect();
+            	return true;
+            }
+            
+		} catch (Exception e) {
+			e.printStackTrace();
+			circle.setVisibility(View.INVISIBLE);
+			return false;
+		}
+		
+		return false;
+
+
+	}
 }
