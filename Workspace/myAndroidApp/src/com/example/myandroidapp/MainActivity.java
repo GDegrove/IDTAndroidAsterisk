0a4f07a7887e8a1de9e8b95631781280eb20ec9a
Jérémie Vincke <jeremie_vincke@hotmail.com>
2012-12-06 17:42:36 +0100

    Commit Jerem : application Android finale
    Connexion - reload - déconnexion - show registred users

--- Workspace/myAndroidApp/src/com/example/myandroidapp/MainActivity.java
+++ Workspace/myAndroidApp/src/com/example/myandroidapp/MainActivity.java
@@ -1,35 +1,36 @@
 package com.example.myandroidapp;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.CharArrayBuffer;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	Socket socket;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		
+			init();
             	
-            	View   text_ip        = findViewById(R.id.text_ip);
-            	View   button_connect = findViewById(R.id.button_connect);
-            	View   text_connected = findViewById(R.id.text_connected);
-            	
-            	View   progressCircle = findViewById(R.id.progressCircle);
-            	
-            	progressCircle.setVisibility(View.INVISIBLE);
-            	text_connected.setVisibility(View.INVISIBLE);
-            	
-            	
-            	button_connect.setOnClickListener(clickListener);
             	           	
 	}
 
@@ -40,61 +41,211 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	OnClickListener clickListener = new OnClickListener() {
+	public void init() {
+		
+		
+    	(findViewById(R.id.text_connected)).setVisibility(View.GONE);
+    	(findViewById(R.id.button_reload)).setVisibility(View.GONE);    	
+    	
+    	View button_reload = findViewById(R.id.button_reload);
+    	View button_disconnect = findViewById(R.id.button_disconnect);
+    	View button_show_users = findViewById(R.id.button_show_users);
+    	button_disconnect.setVisibility(View.GONE);    	
+    	
+    	button_reload.setOnClickListener(clickReloadListener);
+    	button_disconnect.setOnClickListener(clickDisconnectListener);
+    	(findViewById(R.id.button_connect)).setOnClickListener(clickConnectListener);
+    	button_show_users.setVisibility(View.GONE);
+    	button_show_users.setOnClickListener(clickShowUsersListener);
+    	
+    	(findViewById(R.id.text_users)).setVisibility(View.GONE);
+    			
+	}
+	
+	
+	OnClickListener clickConnectListener = new OnClickListener() {
 	    
 	    public void onClick(View v) {
-	    	
 	        System.out.println("CLICKED");
-	        TextView text_ip = (TextView)findViewById(R.id.text_ip);
-	        View progressCircle = findViewById(R.id.progressCircle);
+	        TextView ip = (TextView) findViewById(R.id.TextFieldIP);
+	        TextView id = (TextView) findViewById(R.id.TextFieldID);
+	        TextView pwd = (TextView) findViewById(R.id.TextFieldPWD);
 	        
-	        if(connect(text_ip.getText().toString(), progressCircle)) {
+	        connect(ip.getText().toString(), id.getText().toString(), pwd.getText().toString());
 	        
-	        	View button_connect = findViewById(R.id.button_connect);
-	        	button_connect.setVisibility(View.GONE);
-	        	View text_connected = findViewById(R.id.text_connected);
-            	text_connected.setVisibility(View.VISIBLE);
-            	TextView text_ip_connected = (TextView)findViewById(R.id.text_ip_connected);
-            	text_ip_connected.setVisibility(View.VISIBLE);
-            	
-            	text_ip_connected.setText(text_ip.getText());
-	        }
 	    }
 	};
 	
-	public boolean connect(String ip, View circle) {
+	
+	OnClickListener clickDisconnectListener = new OnClickListener() {
+		
+		public void onClick(View v) {
+			System.out.println("CLICKED");
+			disconnect();			
+		}
+	};
+	
+	OnClickListener clickReloadListener = new OnClickListener() {
+		
+		public void onClick(View v) {
+			System.out.println("CLICKED");
+			String reloadCommand = "Action: Command\r\nCommand: Reload\r\n\r\n";
+			try {
+				(socket.getOutputStream()).write(reloadCommand.getBytes(Charset.forName("UTF-8")));
+				//disconnect();
+				} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	};
+	
+	OnClickListener clickShowUsersListener = new OnClickListener() {
+		public void onClick(View v) {
+			
+			showUsers();
+		}
+	};
+	
+	
+	public boolean connect(String ip, String id, String pwd) {
 		
 		try {
-
-			// Google's IP : 74.125.132.94
-            //URL url = new URL("http://"+params[0]);
-			URL url = new URL("http://"+ip);
-            System.out.println("url created");
-
-            HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
-            urlc.setRequestProperty("User-Agent", "Android Application:"+"v1");
-            urlc.setRequestProperty("Connection", "close");
-            urlc.setConnectTimeout(1000 * 3); // mTimeout is in seconds
-            System.out.println("trying to connect...");
-            circle.setVisibility(View.VISIBLE);
-            urlc.connect();
-            
-            if (urlc.getResponseCode() == 200) {
-            	//	Main.Log("getResponseCode == 200");
-            	System.out.println("got response 200");
-            	circle.setVisibility(View.INVISIBLE);
-            	urlc.disconnect();
-            	return true;
-            }
-            
+						
+			socket = new Socket(ip, 5038);
+			OutputStream os = socket.getOutputStream();
+			InputStream is = socket.getInputStream();
+			
+			//String pkt = "GET /index.html HTTP/1.1\r\nHost: 192.168.1.3\r\nConnection: Keep-Alive\r\n\r\n";
+			//os.write(pkt.getBytes(Charset.forName("UTF_8")));
+			
+			String s = "Action: Login\r\nActionID: 1\r\nUsername: "+id+"\r\nSecret: "+pwd+"\r\n\r\n";
+			
+			//String s = "GET /index.html HTTP/1.1\r\nHost: 192.168.1.3\r\nConnection: Keep-Alive\r\n\r\n";
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			os.write(s.getBytes(Charset.forName("UTF-8")));
+			
+			String serverInfo = br.readLine();
+			System.out.println("Server information : "+serverInfo);
+			
+			for(int i = 1; i < 4; i++)
+				if(is.available() < 1) {
+					Thread.currentThread().sleep(1000);		//wait until data is available
+					if(i == 3) {
+						System.out.println("Request timed out.");
+						return false;
+					}
+				}
+			
+			String result = br.readLine();			
+			System.out.println(result);
+			
+			br.readLine();		//Action id : x
+			
+			String serverMessage = br.readLine();
+			System.out.println(serverMessage);
+			
+			System.out.println("...");			
+			
+			if(result.equals("Response: Error")) {
+				System.out.println("ERROR : incorrect id/password.");
+				socket.close();
+				System.out.println("socket closed.");
+				return false;
+			}
+			else if(result.equals("Response: Success")) {			//successfully connected
+				
+				System.out.println("Successfully logged in...");
+				
+				(findViewById(R.id.textMainMessage)).setVisibility(View.GONE);
+				((TextView)findViewById(R.id.TextFieldIP)).setVisibility(View.GONE);
+				((TextView)findViewById(R.id.TextFieldID)).setVisibility(View.GONE);
+				((TextView)findViewById(R.id.TextFieldPWD)).setVisibility(View.GONE);
+				(findViewById(R.id.button_connect)).setVisibility(View.GONE);
+				
+				(findViewById(R.id.button_disconnect)).setVisibility(View.VISIBLE);
+				(findViewById(R.id.button_reload)).setVisibility(View.VISIBLE);
+				
+				TextView text_connected = (TextView) findViewById(R.id.text_connected);
+				text_connected.setText(getString(R.string.connected)+" "+ip);
+				text_connected.setVisibility(View.VISIBLE);
+				
+				(findViewById(R.id.button_show_users)).setVisibility(View.VISIBLE);
+								
+				return true;
+			}		
+			else {
+				System.out.println("Error while receiving the response from the server.");
+				return false;
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
-			circle.setVisibility(View.INVISIBLE);
-			return false;
+			}
+		return false;
+	}
+	
+	public void disconnect() {
+		
+		try {
+			String logoff = "Action: Logoff\r\n\r\n";
+			socket.getOutputStream().write(logoff.getBytes(Charset.forName("UTF-8")));
+			
+			socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("Error while trying to log off.");
+			}
+		
+		(findViewById(R.id.text_users)).setVisibility(View.GONE);
+		
+		(findViewById(R.id.textMainMessage)).setVisibility(View.VISIBLE);
+		(findViewById(R.id.TextFieldID)).setVisibility(View.VISIBLE);
+		(findViewById(R.id.TextFieldIP)).setVisibility(View.VISIBLE);
+		(findViewById(R.id.TextFieldPWD)).setVisibility(View.VISIBLE);
+		(findViewById(R.id.button_connect)).setVisibility(View.VISIBLE);
+		((TextView)findViewById(R.id.TextFieldID)).setText("");
+		((TextView)findViewById(R.id.TextFieldPWD)).setText("");
+		
+		init();
+	}
+	
+	public void showUsers() {
+		
+		String commandShowUsers = "Action: Command\r\nCommand: sip show peers\r\n\r\n";
+		try {
+			(socket.getOutputStream()).write(commandShowUsers.getBytes(Charset.forName("UTF-8")));
+			Thread.currentThread().sleep(1000);
+			
+			String global = "";
+			
+			InputStream is = socket.getInputStream();
+			while(is.available() > 0) {
+				System.out.println("Data vailable");
+				BufferedReader br = new BufferedReader(new InputStreamReader(is));
+				global = global+br.readLine();
+				global = global+br.readLine();
+				global = global+br.readLine();
+				global = global+br.readLine();
+				global = global+br.readLine();
+				global = global+br.readLine();
+				global = global+br.readLine();
+				global = global+br.readLine();
+				System.out.println(global);
+				TextView textZone = (TextView)findViewById(R.id.text_users);
+				textZone.setVisibility(View.VISIBLE);
+				textZone.setMovementMethod(new ScrollingMovementMethod());
+				textZone.setText(global);
+			}
+			
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		
-		return false;
-
-
+		
 	}
+	
 }
